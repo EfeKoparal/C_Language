d43177a71ac94e087f6074bda3f1cf3312c9685e
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:20:20 +0300

    Create float.c

---
+++ float.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+    
+    
+    //float degiskenler
+    
+    //%f 
+    
+    float sayi1,sayi2;
+    
+    sayi1 = 5;
+    sayi2 = 2;
+    
+    printf("%f",sayi1/sayi2);
+    
+    
+    return 0;
+}
