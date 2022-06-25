75a5ab09abf250e157c7ca0288ee7b290b4c821c
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:20:00 +0300

    Create and(ampersand).c

---
+++ and(ampersand).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int sayi;
+    printf("Bir sayi giriniz:");
+    scanf("%d",&sayi);
+    
+    printf("girmis oldugunuz sayi %d",sayi);
+
+
+    return 0;
+}
