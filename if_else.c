b40325d672533fee905f3876995bed81ae4ab57e
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:21:16 +0300

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    
+    
+    int sayi = 5;
+    
+    if(sayi == 5)
+    {
+        printf("dogru");
+        
+    }
+    else
+    {
+        printf("yanlis");
+    }
+    return 0;
+}
