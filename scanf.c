48cda828033bcf76e2c7869c5498d2aa846680e7
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:18:04 +0300

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    char kitapad[30];
+    
+    printf("Kitap adi giriniz : ");
+    scanf("%s",kitapad);
+
+    printf("%s in sifresi = 12345 (:D)",kitapad);
+    
+    return 0;
+}
