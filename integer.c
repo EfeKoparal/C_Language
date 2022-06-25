3a0b76d2f011e78ceda52e1e9c63c03bf896443e
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:19:03 +0300

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+    
+    
+    int sayi;
+    sayi = 6;
+    printf("sayimiz: %d",sayi);
+    
+    
+    /////////////////////////////////////
+    
+    
+    int sayi1 = 5;
+    int sayi2 = 10;
+    
+    
+    printf("iki sayinin toplami = 2*(%d + %d) = %d",sayi1,sayi2,2*(sayi1+sayi2));
+    
+    
+    return 0;
+}
