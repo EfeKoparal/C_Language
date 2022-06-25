044ee2554f39e2c1ae14723667b90d242e98627b
Efe Koparal <efekoparal@proton.me>
2022-06-25 16:17:24 +0300

    Create char.c

---
+++ char.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+/*
+    char kitapadi[20] = "Mufettis";
+    char kitapyazar[10] = "Gogol";
+    
+    printf("%s\n",kitapadi);
+    printf("%s",kitapyazar);
+
+*/
+
+    char kitaptur[10] = "tiyaro";
+    char kitaptur1[10] = "oyunu";    
+    printf("Kitap turu: %s %s",kitaptur,kitaptur1);
+    
+    
+    return 0;
+}
