ab1b3ded0b9a5c36c7e412ebeadf6d583a8b2b2c
Efe Koparal <efekoparal@proton.me>
2022-07-10 19:06:34 +0300

    Create 20_while.c

---
+++ 20_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+    
+    int i;
+    i=0;
+    
+    while(i <= 10){
+        printf("%d merhaba dunya(yeterlenartik)\n",i);
+        
+        i++;
+    }
+    return 0;
+}
